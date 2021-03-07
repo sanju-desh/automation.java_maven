a7092d275f1165c36fcc8afcd9a1a3db0bdda226
Sanjeevani <sanju.desh@gmail.com>
2021-03-06 18:48:59 -0600

    driver initialize-part1

--- src/main/java/org/automation/resources/Base.java
+++ src/main/java/org/automation/resources/Base.java
@@ -1,7 +1,19 @@
 package org.automation.resources;
-
-import org.openqa.selenium.WebDriver;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
 public class Base {
-    WebDriver driver;
+    //WebDriver driver;
+
+    public void initializeDriver() throws IOException {
+        Properties properties = new Properties();
+        String propPath = "/src/main/java/org.automation/resources/prop.properties";
+        FileInputStream fs = new FileInputStream("user.dir"+propPath);
+        properties.load(fs);
+        String driverPath = properties.getProperty("chromedriver");
+        System.out.println(driverPath);
+    }
+
 }
