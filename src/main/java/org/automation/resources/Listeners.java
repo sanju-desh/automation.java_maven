a4723b736004da5bb2986e479870d4b7dce52702
cdoss-lifesize <cdoss@lifesize.com>
2021-03-08 23:40:28 -0600

    Adding reporter

--- src/main/java/org/automation/resources/Listeners.java
+++ src/main/java/org/automation/resources/Listeners.java
@@ -1,5 +1,8 @@
 package org.automation.resources;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 import org.apache.logging.log4j.*;
 import org.apache.logging.log4j.Logger;
 import org.testng.ITestContext;
@@ -7,26 +10,33 @@ import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 public class Listeners implements ITestListener {
-
+    private static ExtentReports report = Reporter.getExtentReport();
     private static Logger log = LogManager.getLogger(Listeners.class.getName());
+    ExtentTest test;
+    ThreadLocal<ExtentTest> extentTest = new ThreadLocal<ExtentTest>();
 
     @Override
     public void onTestStart(ITestResult result) {
+        test = report.createTest(result.getMethod().getMethodName());
+        extentTest.set(test);
         log.info(result.getName() + " test has started");
     }
 
     @Override
     public void onTestSuccess(ITestResult result) {
+        extentTest.get().log(Status.PASS, result.getMethod().getMethodName() + " test has passed");
         log.info(result.getName() + " test has passed");
     }
 
     @Override
     public void onTestFailure(ITestResult result) {
+        extentTest.get().log(Status.FAIL, result.getMethod().getMethodName() + " test has failed");
         log.error(result.getName() + " test has failed");
     }
 
     @Override
     public void onTestSkipped(ITestResult result) {
+        extentTest.get().log(Status.SKIP, result.getMethod().getMethodName() + " test is skipped");
         log.info(result.getName() + " test is skipped");
     }
 
@@ -42,11 +52,10 @@ public class Listeners implements ITestListener {
 
     @Override
     public void onStart(ITestContext context) {
-
     }
 
     @Override
     public void onFinish(ITestContext context) {
-
+        report.flush();
     }
 }
